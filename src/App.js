f39d534f2b60852f102311ab1602904e5b3642e2
sonuchaurasiya7633 <sonukumar763303@gmail.com>
2025-03-30 12:44:29 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import Nav from './Nav'
-import Section from './Section'
-import Footer from './Footer'
-function App(){
+import React from 'react';
+import Nav from './Nav';
+import Section from './Section';
+import Footer from './Footer';
+
+function App() {
   return (
     <div>
-      <Nav></Nav>
-      <Section></Section>
-      <Footer></Footer>     
+      <Nav />
+      <Section />
+      <Footer />
     </div>
-  )
+  );
 }
-export default App
+
+export default App;
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,57 +1,61 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const menus = [
+  { Label: 'Home', Path: '/' },
+  { Label: 'Image', Path: '/image' },
+  { Label: 'Movies', Path: '/movies' },
+  { Label: 'Videos', Path: '/videos' },
+  { Label: 'Contact Us', Path: '/contact-us' },
+];
 
 const Nav = () => {
   return (
-    <div>
-       <nav style={{
-        width:'100%',
-        margin:0,
-        padding:0,
-        height:60,
-        backgroundColor:"black",
-        border:"3px solid red"
- 
-      }}>
-       <ul style={{
-        paddingTop:17,
-        margin:0,
-        listStyle:"none",
-        display:"flex",
-        justifyContent:'space-evenly',
-      
-       }}>
-       <li><a href="#" style={{
-        textDecoration:"none",
-        color:"white",
-        display:"block"
-       }}>Home</a></li>
-       <li><a href="#" style={{
-        textDecoration:"none",
-        color:"white",
-   
-        display:"block"
-       }}>Images</a></li>
-       <li><a href="#" style={{
-        textDecoration:"none",
-        color:"white",
-        
-        display:"block"
-       }}>Videos</a></li>
-       <li><a href="#" style={{
-        textDecoration:"none",
-        color:"white",
-      
-        display:"block"
-       }}>Movies</a></li>
-       <li><a href="#" style={{
-        textDecoration:"none",
-        color:"white",
-        display:"block"
-       }}>Contact Us</a></li>
-       </ul>
-      </nav>
-    </div>
-  )
-}
+    <nav
+      style={{
+        width: '100%',
+        height: '60px',
+        backgroundColor: 'black',
+        display: 'flex',
+        alignItems: 'center',
+        margin:25,
+        borderBottom: '3px solid red',
+      }}
+    >
+      <ul
+        style={{
+          listStyle: 'none', 
+          display: 'flex',
+          justifyContent: 'space-evenly',  
+          alignItems: 'center',
+          width: '100%',
+          margin: 0,
+          padding: 0,
+        }}
+      >
+        {menus.map((menu, index) => (
+          <li key={index}>
+            <Link
+              to={menu.Path}
+              style={{
+                textDecoration: 'none',
+                color: 'white',
+                fontSize: '18px',
+                fontWeight: 'bold',
+                padding: '10px 15px',
+                borderRadius: '5px',
+                transition: 'background-color 0.3s',
+              }}
+              onMouseEnter={(e) => (e.target.style.backgroundColor = 'red')}
+              onMouseLeave={(e) => (e.target.style.backgroundColor = 'transparent')}
+            >
+              {menu.Label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
